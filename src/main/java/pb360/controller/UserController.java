3f234384f40e05fd1901a1a97d104a9626c77bdc
= <=>
2018-07-02 17:57:40 +0700

    updated

--- src/main/java/pb360/controller/UserController.java
+++ src/main/java/pb360/controller/UserController.java
@@ -12,12 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-=======
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
->>>>>>> e86a4b8932c5bc7d31e682745d6d0afa4602f09b
 
 import pb360.model.MessageObject;
 import pb360.model.UserModel;
@@ -35,16 +30,10 @@ public class UserController {
 	public HttpEntity<MessageObject> createNewUser(@Valid @RequestBody UserModel user) {
 		MessageObject messageObject = userService.register(user);
 		if (messageObject != null) {
-<<<<<<< HEAD
-			messageObject.add(linkTo(methodOn(UserController.class).getClass()).withSelfRel());
-			return new ResponseEntity<MessageObject>(messageObject, HttpStatus.OK);
-		}
-=======
 			messageObject.add(linkTo(methodOn(UserController.class).getClass()).withRel(user.getUsername()));
 			return new ResponseEntity<MessageObject>(messageObject, HttpStatus.OK);
 		}
 
->>>>>>> e86a4b8932c5bc7d31e682745d6d0afa4602f09b
 		return new ResponseEntity<MessageObject>(HttpStatus.NO_CONTENT);
 	}
 
@@ -54,16 +43,6 @@ public class UserController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/{username}")
-<<<<<<< HEAD
-	public HttpEntity<MessageObject> loginUser(@PathVariable("username") String username, @RequestBody UserModel user) {
-		MessageObject messageObject = userService.login(username, user);
-		if (messageObject != null ) {
-			messageObject.add(linkTo(methodOn(this.getClass()).getClass()).withRel(username));
-			return new ResponseEntity<MessageObject>(messageObject, HttpStatus.OK);
-		}
-
-		return new ResponseEntity<MessageObject>(messageObject, HttpStatus.NO_CONTENT);
-=======
 	public HttpEntity<MessageObject> loginUser(@PathVariable("username") String username,
 			@RequestBody UserModel userModel) {
 		MessageObject messageObject = userService.login(username, userModel);
@@ -73,6 +52,5 @@ public class UserController {
 		}
 
 		return new ResponseEntity<MessageObject>(HttpStatus.NO_CONTENT);
->>>>>>> e86a4b8932c5bc7d31e682745d6d0afa4602f09b
 	}
 }
