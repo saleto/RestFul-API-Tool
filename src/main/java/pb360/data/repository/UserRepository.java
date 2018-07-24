09a853527fff588b73491792a66b0c3955a9210a
= <=>
2018-07-24 16:57:25 +0700

    I add getPager() in UserService, return total pages, current page, auto deliver

--- src/main/java/pb360/data/repository/UserRepository.java
+++ src/main/java/pb360/data/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package pb360.data.repository;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import pb360.data.entity.UserEntity;
 
-public interface UserRepository extends MongoRepository<UserEntity, String> {
+public interface UserRepository
+		extends MongoRepository<UserEntity, String>, PagingAndSortingRepository<UserEntity, String> {
 
 	public UserEntity findOneByPassword(String username, String password);
 
 	public UserEntity findOneByUsername(String username);
 
 	public UserEntity findByUsername(String username);
+
+
 }
